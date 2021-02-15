f1529ec458f0bbf39a57916bb031ed4ed72012cd
evg-cv <evgenijkolotov333@gmail.com>
2021-02-15 18:29:24 +0300

    pi cam testing

--- pi_cam_t_k.py
+++ pi_cam_t_k.py
@@ -2,17 +2,20 @@ import os
 import cv2
 import time
 
-from picamera import PiCamera
+# from picamera import PiCamera
+from imutils.video import VideoStream
 
 
-capture = PiCamera(framerate=30)
+cap = VideoStream(usePiCamera=True).start()
+# capture = PiCamera(framerate=30)
 time.sleep(2)
-capture.awb_mode = "incandescent"
-tmp_path = os.path.join('/tmp', 'temp.jpg')
+cap.awb_mode = "incandescent"
+# tmp_path = os.path.join('/tmp', 'temp.jpg')
 
 while True:
-    capture.capture(tmp_path)
-    frame = cv2.imread(tmp_path)
+    frame = cap.read()
+    # capture.capture(tmp_path)
+    # frame = cv2.imread(tmp_path)
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xff == ord("q"):
         break
