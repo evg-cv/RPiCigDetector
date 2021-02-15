b677b89f68ec51aca064d92678bb6d791545d9c4
evg-cv <evgenijkolotov333@gmail.com>
2021-02-15 17:55:59 +0300

    pi cam testing

--- pi_cam_t_k.py
+++ pi_cam_t_k.py
@@ -7,7 +7,7 @@ capture = PiCamera(framerate=30)
 capture.awb_mode = "fluorescent"
 
 while True:
-    _, frame = capture.capture()
+    frame = capture.capture()
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) & 0xff == ord("q"):
         break
